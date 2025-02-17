37ba2748d17701e21995505efec66998b4848218
star_moon_cloud_k <kspecial44@gmail.com>
2025-02-17 23:23:12 +0900

    FEAT : basic layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,30 +1,41 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono } from "next/font/google";
 
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
+import "@workspace/ui/globals.css";
+import { Providers } from "@/components/providers";
+import { AppSidebar } from "@/components/app-side";
+import {
+  SidebarProvider,
+  SidebarTrigger,
+} from "@workspace/ui/components/sidebar";
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
-        <Providers>{children}</Providers>
+        <SidebarProvider>
+          <AppSidebar />
+          <Providers>
+            <SidebarTrigger className="w-14 h-14" />
+            <div className="flex-1">{children}</div>
+          </Providers>
+        </SidebarProvider>
       </body>
     </html>
-  )
+  );
 }
