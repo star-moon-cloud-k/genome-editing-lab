e992bb077dc3f82db77d45d94f8141f245fc63c3
star_moon_cloud_k <kspecial44@gmail.com>
2025-02-18 16:29:28 +0900

    MODIFY : main page table

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,14 @@
 import MainTable from "@/components/stock-table";
 import { Button } from "@workspace/ui/components/button";
 import { main } from "@config/main.json";
+import ShortfallTable from "@/components/shortfall-table";
 
 export default function Page() {
   return (
     <div className="flex-1 w-full items-center justify-center min-h-svh ">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold mt-5">{main.tableName}</h1>
+        <ShortfallTable />
       </div>
     </div>
   );
--- apps/web/components/shortfall-table.tsx
+++ apps/web/components/shortfall-table.tsx
@@ -13,31 +13,35 @@ import {
 import { FlaskConicalOff } from "lucide-react";
 import Image from "next/image";
 
-export default function StockTable() {
+export default function ShortfallTable() {
   return (
     <div className="m-5 relative w-100">
       <Table className="table-fixed">
         <TableHeader>
           <TableRow>
-            <TableHead className="">image</TableHead>
             <TableHead className="">Name</TableHead>
             <TableHead>Amount</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           <TableRow>
-            <TableCell className="font-medium">
-              <FlaskConicalOff className="stroke-muted" size={"100"} />
-            </TableCell>
-
             <TableCell>
-              <div>price</div>
-              <div>
-                <Badge className="bg-green-600">:)</Badge>
-              </div>
+              <div>sdsd</div>
+              <div></div>
             </TableCell>
             <TableCell className="flex-row">
-              <Button variant={"ghost"}>5</Button> {/** 이 쪽 라인 */}
+              <text className="mr-5">5</text>
+              <Badge className="bg-red-600">:(</Badge>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <div>price</div>
+              <div></div>
+            </TableCell>
+            <TableCell className="flex-row">
+              <text className="mr-5">5</text>
+              <Badge className="bg-slate-600">:0</Badge>
             </TableCell>
           </TableRow>
         </TableBody>
